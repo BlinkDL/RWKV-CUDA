a0a7ffdfb3dd077c013a63d82e5b941286636d81
BlinkDL <a@a.com>
2023-09-07 20:10:12 +0000

    rwkv5 backward (partial)

--- wkv5/run.py
+++ wkv5/run.py
@@ -12,10 +12,11 @@ torch.backends.cuda.matmul.allow_tf32 = False
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 DEVICE = 'cuda'
-CUDA_KERNEL_VERSION = '1b'
+CUDA_KERNEL_VERSION = '1c'
 
 '''
 python run.py correctness && python run.py correctness_more && python run.py benchmark
+python run.py backward
 '''
 JOB = sys.argv[1].strip()
 
@@ -91,30 +92,67 @@ def RUN_FORMULA_2(B, T, C, H, r, k, v, w, u):
 
     return out.view(B, T, C)
 
+def RUN_BACKWARD_1(B, T, C, H, gy, r, k, v, w, u):
+    N = C // H
+    gy = gy.view(B, T, H, N)
+    r = r.view(B, T, H, N)
+    k = k.view(B, T, H, N)
+    v = v.view(B, T, H, N)
+    w = w.view(H, N)
+    u = u.view(H, N)
+
+    gr = torch.zeros((B, T, H, N), device=DEVICE)    
+    gk = torch.zeros((B, T, H, N), device=DEVICE)
+    gv = torch.zeros((B, T, H, N), device=DEVICE)
+
+    for b in range(B):
+        for h in range(H):
+            for t in range(T):
+                for n in range(N):
+                    for nn in range(N):
+
+                        for tt in range(t+1):
+                            ww = u[h,n] if (tt == t) else w[h,n] ** (t - tt - 1)
+                            gr[b,t,h,n] += ww * k[b,tt,h,n] * v[b,tt,h,nn] * gy[b,t,h,nn]
+
+                        for tt in range(t,T):
+                            ww = u[h,n] if (tt == t) else w[h,n] ** (tt - t - 1)
+                            gk[b,t,h,n] += r[b,tt,h,n] * ww * v[b,t,h,nn] * gy[b,tt,h,nn]
+
+                            ww = u[h,nn] if (tt == t) else w[h,nn] ** (tt - t - 1)
+                            gv[b,t,h,n] += r[b,tt,h,nn] * ww * k[b,t,h,nn] * gy[b,tt,h,n]
+
+    return gr.view(B, T, C), gk.view(B, T, C), gv.view(B, T, C)
+
 ######################################################################################################
 # CUDA kernel
 ######################################################################################################
 
-if JOB == 'correctness':
+if JOB == 'correctness' or JOB == 'backward':
     # B = 16
     # T = 4
     # C = 16
     # HEAD_SIZE = 4
 
-    B = 2
-    T = 4
-    C = 16
-    HEAD_SIZE = 4
+    # B = 2
+    # T = 4
+    # C = 16
+    # HEAD_SIZE = 4
 
-    # B = 1
-    # T = 2
-    # C = 2
-    # HEAD_SIZE = 1
+    # B = 2
+    # T = 3
+    # C = 4
+    # HEAD_SIZE = 2
+
+    B = 1
+    T = 3
+    C = 2
+    HEAD_SIZE = 1
 else:
-    HEAD_SIZE = 64
     B = 8
     T = 4096
     C = 4096
+    HEAD_SIZE = 64
 
 H = C // HEAD_SIZE
 
@@ -230,26 +268,49 @@ def CHECK_CORRECT():
     else:
 
         y0 = RUN_FORMULA_1(B, T, C, H, r, k, v, w, u)
-        print('result', val(y0), '\n\n')
+        print(f'result\n{val(y0)}\n')
 
         y1 = RUN_FORMULA_2(B, T, C, H, r, k, v, w, u)
-        print('result', val(y1), '\n\n')
+        print(f'result\n{val(y1)}\n')
 
         y2 = RUN_CUDA(B, T, C, H, r, k, v, w, u)
-        print('result', val(y2), '\n\n')
+        print(f'result\n{val(y2)}\n')
 
         print('--> correct =', torch.allclose(y0, y1), torch.allclose(y0, y2),
             ', err ratio =', get_err_ratio(y0, y1), get_err_ratio(y0, y2))
 
-    # # a strange loss for better verification
-    # loss0 = ((y0 * y0) - torch.tanh(y0)).sum()
-    # loss0.backward()
-    # gr0 = r.grad.data.clone()
-    # gk0 = k.grad.data.clone()
-    # gv0 = v.grad.data.clone()
-    # gw0 = w.grad.data.clone()
-    # gu0 = u.grad.data.clone()
-    # print(gr0, gk0, gv0, gw0, gu0)
+        
+        if JOB == 'backward':
+
+            def LOSS(y): # a strange loss for better verification
+                return ((y * y) - torch.tanh(y)).sum()
+
+            yy = y0.clone().detach().requires_grad_(True)
+            LOSS(yy).backward()
+            gy = yy.grad.data.clone()
+            print(f'grad_y\n{val(gy)}\n')
+
+            LOSS(y0).backward()
+            gr0 = r.grad.data.clone()
+            gk0 = k.grad.data.clone()
+            gv0 = v.grad.data.clone()
+            gw0 = w.grad.data.clone()
+            gu0 = u.grad.data.clone()
+            print(f'g_r0\n{val(gr0)}\n')
+            print(f'g_k0\n{val(gk0)}\n')
+            print(f'g_v0\n{val(gv0)}\n')
+            print(f'g_w0\n{val(gw0)}\n')
+            print(f'g_u0\n{val(gu0)}\n')
+
+            gr1, gk1, gv1 = RUN_BACKWARD_1(B, T, C, H, gy, r, k, v, w, u)
+            print(f'g_r1\n{val(gr1)}\n')
+            print(f'g_k1\n{val(gk1)}\n')
+            print(f'g_v1\n{val(gv1)}\n')
+
+            print('--> g_r correct =', torch.allclose(gr0, gr1), ', err ratio =', get_err_ratio(gr0, gr1))
+            print('--> g_k correct =', torch.allclose(gk0, gk1), ', err ratio =', get_err_ratio(gk0, gk1))
+            print('--> g_v correct =', torch.allclose(gv0, gv1), ', err ratio =', get_err_ratio(gv0, gv1))
+
 
 def CHECK_SPEED(silent=False):
     print('B', B, 'T', T, 'C', C, 'H', H)
@@ -270,7 +331,7 @@ def CHECK_SPEED(silent=False):
 
 if __name__ == "__main__":
 
-    if JOB == 'correctness':
+    if JOB == 'correctness' or JOB == 'backward':
         print(f'\n\nCheck CUDA kernel v{CUDA_KERNEL_VERSION} correctness...')
         CHECK_CORRECT()
     elif JOB == 'correctness_more':
