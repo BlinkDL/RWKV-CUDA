f9f678e9515742370d5c927fd12af83a4a42ac2e
BBuf <1182563586@qq.com>
2023-09-07 15:25:43 +0000

    shared memory for state

--- wkv5/cuda/wkv5_cuda_v4.cu
+++ wkv5/cuda/wkv5_cuda_v4.cu
@@ -6,57 +6,57 @@ __global__ void kernel_forward(const int B, const int T, const int C, const int 
                                       const F *__restrict__ const _r, const F *__restrict__ const _k, const F *__restrict__ const _v, const F *__restrict__ const _w, const F *__restrict__ const _u,
                                       F *__restrict__ const _y)
 {
-    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    const int _b = idx / C;
-    const int _h = (idx / N) - ((idx / N) /  H) * H;
-    const int _i = idx - (idx / N) * N;
+    extern __shared__ float4 s_state[N >> 2];
+  
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int _b = idx / C;
+  const int _h = (idx / N) - ((idx / N) /  H) * H;
+  const int _i = idx - (idx / N) * N;
 
-    const int _o0 = _b * T * C + _h * N;
-    const int _o1 = _h * N;
+  const int _o0 = _b * T * C + _h * N; 
+  const int _o1 = _h * N;
 
-    const float4 *__restrict__ const k = (float4 *)(_k + _o0);
-    const float4 *__restrict__ const r = (float4 *)(_r + _o0);
-    const float4 *__restrict__ const w = (float4 *)(_w + _o1);
-    const float4 *__restrict__ const u = (float4 *)(_u + _o1);
-    const F *__restrict__ const v = _v + _o0 + _i;
-    F *__restrict__ const y = _y + _o0 + _i;
+  const float4 *__restrict__ const k = (float4 *)(_k + _o0);
+  const float4 *__restrict__ const r = (float4 *)(_r + _o0);
+  const float4 *__restrict__ const w = (float4 *)(_w + _o1);
+  const float4 *__restrict__ const u = (float4 *)(_u + _o1);
+  const F *__restrict__ const v = _v + _o0 + _i;
+  F *__restrict__ const y = _y + _o0 + _i;
 
-    __align__(16) float4 state[N >> 2] = { make_float4(0.0f, 0.0f, 0.0f, 0.0f) };
+  for (int __t = 0; __t < T; __t++)
+  {
+    const int _t = __t * (C >> 2);
+    const int tt = __t * C;
+    const F vv = v[tt];
+    float4 result = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
 
-    for (int __t = 0; __t < T; __t++)
+    for (int _j = 0; _j < N >> 2; _j++)
     {
-        const int _t = __t * (C >> 2);
-        const int tt = __t * C;
-        const F vv = v[tt];
-        float4 result = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
+      const int j = _t + _j;
 
-        for (int _j = 0; _j < N >> 2; _j++)
-        {
-            const int j = _t + _j;
+      const float4 k_val = k[j];
+      const float4 r_val = r[j];
+      float4 x;
+      x.x = k_val.x * vv;
+      x.y = k_val.y * vv; 
+      x.z = k_val.z * vv;
+      x.w = k_val.w * vv;
 
-            const float4 k_val = k[j];
-            const float4 r_val = r[j];
-            float4 x;
-            x.x = k_val.x * vv;
-            x.y = k_val.y * vv;
-            x.z = k_val.z * vv;
-            x.w = k_val.w * vv;
+      float4 s = s_state[_j];
 
-            float4 s = state[_j];
+      result.x += r_val.x * (u[_j].x * x.x + s.x);
+      result.y += r_val.y * (u[_j].y * x.y + s.y);
+      result.z += r_val.z * (u[_j].z * x.z + s.z);
+      result.w += r_val.w * (u[_j].w * x.w + s.w);
 
-            result.x += r_val.x * (u[_j].x * x.x + s.x);
-            result.y += r_val.y * (u[_j].y * x.y + s.y);
-            result.z += r_val.z * (u[_j].z * x.z + s.z);
-            result.w += r_val.w * (u[_j].w * x.w + s.w);
-
-            state[_j].x = s.x * w[_j].x + x.x;
-            state[_j].y = s.y * w[_j].y + x.y;
-            state[_j].z = s.z * w[_j].z + x.z;
-            state[_j].w = s.w * w[_j].w + x.w;
-        }
-
-        y[tt] += result.x + result.y + result.z + result.w;
+      s_state[_j].x = s.x * w[_j].x + x.x;
+      s_state[_j].y = s.y * w[_j].y + x.y;
+      s_state[_j].z = s.z * w[_j].z + x.z;
+      s_state[_j].w = s.w * w[_j].w + x.w;
     }
+
+    y[tt] += result.x + result.y + result.z + result.w; 
+  }
 }
 
 template <typename F>
