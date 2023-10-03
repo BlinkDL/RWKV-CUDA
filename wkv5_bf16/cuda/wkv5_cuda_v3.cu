997f44acee6d03036401062fedbc2a26dc2d571b
BBuf <1182563586@qq.com>
2023-10-03 12:37:59 +0000

    optimize v2

--- wkv5_bf16/cuda/wkv5_cuda_v3.cu
+++ wkv5_bf16/cuda/wkv5_cuda_v3.cu
@@ -1,130 +1,91 @@
-// Forward Origin Author: Bleatan
-#include <assert.h>
 #include <stdio.h>
-#include <torch/extension.h>
-
+#include <assert.h>
+#include "ATen/ATen.h"
 typedef at::BFloat16 bf16;
 
-constexpr int N = _N_;
-constexpr int PARALLEL_SCAN_BLOCK = 512;
-constexpr int TILE_T = 16;
+template<typename T>
+struct alignas(8 * sizeof(T)) Pack8X {
+  T x, y, z, w, a, b, c, d;
+};
 
-__global__ void kernel_forward_accumulate_state(const int nblocks_per_sample,
-                                                float *__restrict__ state,
-                                                const float *__restrict__ w) {
-  const int b = blockIdx.x, B = gridDim.x;
-  const int h = blockIdx.y / N, H = gridDim.y / N, HNN = H * N * N;
-  const int i = blockIdx.y % N;
-  const int j = threadIdx.x;
+template <typename F>
+__global__ void kernel_forward(const int B, const int T, const int C, const int H,
+                               F *__restrict__ _r, F *__restrict__ _k, const F *__restrict__ const _v, const float *__restrict__ _w, F *__restrict__ _u,
+                               F *__restrict__ const _y)
+{
+    const int b = blockIdx.x / H;
+    const int h = blockIdx.x - (blockIdx.x / H) * H;
+    const int i = threadIdx.x;
+    _w += h*_N_;
+    _u += h*_N_;
 
-  float wp = powf(w[h * N + i], PARALLEL_SCAN_BLOCK);
-  state += (((b * nblocks_per_sample * H + h) * N) + j) * N + i;
-  float s = 0;
+    Pack8X<F>* k = reinterpret_cast<Pack8X<F>*>(_k);
+    Pack8X<F>* r = reinterpret_cast<Pack8X<F>*>(_r);
+    const float4 *__restrict__ const w = (float4 *)(_w);
+    Pack8X<F>* u = reinterpret_cast<Pack8X<F>*>(_u);
+    const F *__restrict__ const v = _v;
+    F *__restrict__ const y = _y;
 
-  for (int block = 0; block < nblocks_per_sample; ++block) {
-    const float next_s = s * wp + *state;
-    *state = s;
-    s = next_s;
-    state += HNN;
-  }
-}
-
-template <bool DoY, typename F>
-__global__ void
-kernel_forward(const int T, const F *__restrict__ const _r,
-               const F *__restrict__ const _k, const F *__restrict__ const _v,
-               const float *__restrict__ _w, const F *__restrict__ _u,
-               F *__restrict__ const _y, float *__restrict__ state) {
-  const int b = blockIdx.x, B = gridDim.x;
-  if (!DoY && b == B - 1)
-    return;
-
-  const int h = blockIdx.y, H = gridDim.y;
-  const int i = threadIdx.x;
-  _w += h * N;
-  _u += h * N;
-
-  __shared__ float rr[TILE_T][N], kk[TILE_T][N];
-
-  state += (b * H + h) * N * N;
-
-  if constexpr (!DoY)
-    for (int j = 0; j < N; ++j)
-      state[j * N + i] = 0;
-
-  for (int _t = b * T * H * N + h * N + i,
-           _tend = (b + 1) * T * H * N + h * N + i;
-       _t < _tend; _t += TILE_T * H * N) {
-    float yy[TILE_T], vv[TILE_T];
-#pragma unroll(TILE_T)
-    for (int tt = 0, t = _t; tt < TILE_T; (++tt), (t += H * N)) {
-      vv[tt] = (float)_v[t];
-      if constexpr (DoY) {
-        yy[tt] = 0.f;
-        rr[tt][i] = (float)_r[t];
-      }
-      kk[tt][i] = (float)_k[t];
+    __align__(16) float4 state[_N_ >> 2] = { make_float4(0.0f, 0.0f, 0.0f, 0.0f) };
+    for ( int  i =0 ;i < _N_ >> 2; i++){
+        state[i] = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
     }
+    __shared__ Pack8X<F> rr[_N_ >> 3], kk[_N_ >> 3];
 
-    __syncthreads();
+    for (int _tt = b*T*C + h*_N_ + i, _tend = (b+1)*T*C + h*_N_ + i; _tt < _tend; _tt += C)
+    {
+        const int _t = _tt >> 3;
+        const float vv = float(v[_tt]);
+        float yy = 0.0;
+        
+        __syncthreads();
+        rr[i >> 3] = r[_t];
+        kk[i >> 3] = k[_t];
+        __syncthreads();
 
-    for (int j = 0; j < N; j += 1) {
-      float s = state[j * N + i];
-      float w = _w[j];
-      float u = (float)_u[j];
+        #pragma unroll
+        for (int j = 0; j < _N_ >> 3; j++)
+        {
+            const Pack8X<F> uu = u[j];
+            const Pack8X<F> rrr = rr[j];
+            const Pack8X<F> kkk = kk[j];
 
-#pragma unroll(TILE_T)
-      for (int tt = 0, t = _t; tt < TILE_T; (++tt), (t += H * N)) {
-        float x = kk[tt][j] * vv[tt];
-        if constexpr (DoY) {
-            float yyy = rr[tt][j] * (u * x + s);
-            yy[tt] += yyy;
+            float4 x1;
+            x1.x = float(kkk.x) * vv;
+            x1.y = float(kkk.y) * vv;
+            x1.z = float(kkk.z) * vv;
+            x1.w = float(kkk.w) * vv;
+            float4 &s1 = state[j<<1];
+            const float4 ww1 = w[j<<1];
+
+            yy += float(rrr.x) * (float(uu.x) * x1.x + s1.x);
+            yy += float(rrr.y) * (float(uu.y) * x1.y + s1.y);
+            yy += float(rrr.z) * (float(uu.z) * x1.z + s1.z);
+            yy += float(rrr.w) * (float(uu.w) * x1.w + s1.w);
+            s1.x = s1.x * ww1.x + x1.x;
+            s1.y = s1.y * ww1.y + x1.y;
+            s1.z = s1.z * ww1.z + x1.z;
+            s1.w = s1.w * ww1.w + x1.w;
+
+            float4 x2;
+            x2.x = float(kkk.a) * vv;
+            x2.y = float(kkk.b) * vv;
+            x2.z = float(kkk.c) * vv;
+            x2.w = float(kkk.d) * vv;
+            float4 &s2 = state[j<<1|1];
+            const float4 ww2 = w[j<<1|1];
+            
+            yy += float(rrr.a) * (float(uu.a) * x2.x + s2.x);
+            yy += float(rrr.b) * (float(uu.b) * x2.y + s2.y);
+            yy += float(rrr.c) * (float(uu.c) * x2.z + s2.z);
+            yy += float(rrr.d) * (float(uu.d) * x2.w + s2.w);
+            s2.x = s2.x * ww2.x + x2.x;
+            s2.y = s2.y * ww2.y + x2.y;
+            s2.z = s2.z * ww2.z + x2.z;
+            s2.w = s2.w * ww2.w + x2.w;
         }
-        s = s * w + x;
-      }
-      state[j * N + i] = s;
+        y[_tt] = F(yy);
     }
-
-    if constexpr (DoY)
-#pragma unroll(TILE_T)
-      for (int tt = 0, t = _t; tt < TILE_T; (++tt), (t += H * N))
-        _y[t] = (F)yy[tt];
-
-    __syncthreads();
-  }
-}
-
-void forward(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &r,
-             torch::Tensor &k, torch::Tensor &v, torch::Tensor &w,
-             torch::Tensor &u, torch::Tensor &y) {
-  assert(H * N == C);
-  assert(T % PARALLEL_SCAN_BLOCK == 0);
-  const int blocksz = PARALLEL_SCAN_BLOCK;
-  const int nblocks_per_sample = T / blocksz;
-  const int nblocks = B * nblocks_per_sample;
-  cudaEvent_t events[5];
-  for (int i = 0; i < 5; ++i)
-    cudaEventCreate(&events[i]);
-
-  cudaEventRecord(events[0]);
-  torch::Tensor states =
-      torch::empty({B, nblocks_per_sample, H, N, N}, w.options());
-  cudaEventRecord(events[1]);
-  kernel_forward<false><<<dim3(nblocks, H), dim3(N)>>>(
-      blocksz, r.data_ptr<bf16>(), k.data_ptr<bf16>(), v.data_ptr<bf16>(),
-      w.data_ptr<float>(), u.data_ptr<bf16>(), y.data_ptr<bf16>(),
-      states.data_ptr<float>());
-  cudaEventRecord(events[2]);
-  kernel_forward_accumulate_state<<<dim3(B, H * N), dim3(N)>>>(
-      nblocks_per_sample, states.data_ptr<float>(), w.data_ptr<float>());
-  cudaEventRecord(events[3]);
-  kernel_forward<true><<<dim3(nblocks, H), dim3(N)>>>(
-      blocksz, r.data_ptr<bf16>(), k.data_ptr<bf16>(), v.data_ptr<bf16>(),
-      w.data_ptr<float>(), u.data_ptr<bf16>(), y.data_ptr<bf16>(),
-      states.data_ptr<float>());
-  cudaEventRecord(events[4]);
-
-  cudaEventSynchronize(events[4]);
 }
 
 template <typename F>
@@ -143,7 +104,7 @@ __global__ void kernel_backward(const int B, const int T, const int C, const int
     const float ww = __w[i];
 
     __shared__ float v[_N_], r[_N_], k[_N_], gy[_N_], gy2[_N_], w_[_N_], u_[_N_];    
-    float state[_N_] = {0}, saaaa[_N_] = {0}, sbbbb[_N_] = {0};
+    float state[_N_*2] = {0};
 
     float gw = 0, gu = 0;
     const int t000 = b*T*C + h*_N_ + i;
@@ -175,6 +136,11 @@ __global__ void kernel_backward(const int B, const int T, const int C, const int
         _gr[_t] = F(gr);
     }
     _gu[b*C + h*_N_ + i] = F(gu);
+
+    #pragma unroll
+    for (int j = 0; j < _N_*2; ++j) {
+        state[j] = 0;
+    }
     
     for (int _t = t000; _t < t222; _t += C)
     {
@@ -189,10 +155,12 @@ __global__ void kernel_backward(const int B, const int T, const int C, const int
         for (int j = 0; j < _N_; j++)
         {
             float x = v[j] * k;
-            saaaa[j] = w * (saaaa[j] + sbbbb[j] + x);
-            sbbbb[j] = w * (sbbbb[j] + x);
-            
-            gw += r2 * ww * saaaa[j] * gy2[j];
+            // accum[j] = w[h,i] * (accum[j] + accum[j+N] + x)
+            // accum[j+N] = w[h,i] * (accum[j+N] + x)
+            // gw[h,i] += r[b,t+2,h,i] * _w[h,i] * accum[j] * gy[b,t+2,h,j]
+            state[j] = w * (state[j] + state[j+_N_] + x);
+            state[j+_N_] = w * (state[j+_N_] + x);
+            gw += r2 * ww * state[j] * gy2[j];
         }
     }
     
@@ -200,8 +168,7 @@ __global__ void kernel_backward(const int B, const int T, const int C, const int
 
     #pragma unroll
     for (int j = 0; j < _N_; ++j) {
-        saaaa[j] = 0;
-        sbbbb[j] = 0;
+        state[j] = 0;
     }
 
     __syncthreads();
@@ -218,42 +185,54 @@ __global__ void kernel_backward(const int B, const int T, const int C, const int
         r[i] = float(_r[_t]);
         __syncthreads();
 
-        float gk = 0, gv = 0, x, s;
+        float gk = 0, x, s;
 
         #pragma unroll
         for (int j = 0; j < _N_; j++)
         {
             x = gy[j] * r[i];
-            s = saaaa[j];
-            saaaa[j] = s * w + x;
+            s = state[j];
+            state[j] = s * w + x;
             gk += v[j] * (u * x + s);
-
-            x = gy[i] * r[j];
-            s = sbbbb[j];
-            sbbbb[j] = s * w_[j] + x;
-            gv += k[j] * (u_[j] * x + s);
         }
         _gk[_t] = F(gk);
+    }
+
+    #pragma unroll
+    for (int j = 0; j < _N_; ++j) {
+        state[j] = 0;
+    }
+
+    for (int _t = t111 - C; _t >= t000; _t -= C)
+    {
+        __syncthreads();
+        gy[i] = float(_gy[_t]);
+        r[i] = float(_r[_t]);
+        k[i] = float(_k[_t]);
+        __syncthreads();
+
+        float gv = 0, x, s;
+
+        #pragma unroll
+        for (int j = 0; j < _N_; j++)
+        {
+            x = gy[i] * r[j];
+            s = state[j];
+            state[j] = s * w_[j] + x;
+            gv += k[j] * (u_[j] * x + s);
+        }
         _gv[_t] = F(gv);
     }
+}
+
+void cuda_forward(int B, int T, int C, int H, bf16 *r, bf16 *k, bf16 *v, float *w, bf16 *u, bf16 *y)
+{
+    assert(H*_N_ == C);
+    kernel_forward<<<dim3(B * H), dim3(_N_)>>>(B, T, C, H, r, k, v, w, u, y);
 }
 
 void cuda_backward(int B, int T, int C, int H, bf16 *r, bf16 *k, bf16 *v, float *w, float *ww, bf16 *u, bf16 *gy, bf16 *gr, bf16 *gk, bf16 *gv, bf16 *gw, bf16 *gu)
 {
     assert(H*_N_ == C);
     kernel_backward<<<dim3(B * H), dim3(_N_)>>>(B, T, C, H, r, k, v, w, ww, u, gy, gr, gk, gv, gw, gu);
-}
-
-void backward(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &r, torch::Tensor &k, torch::Tensor &v, torch::Tensor &w, torch::Tensor &ww, torch::Tensor &u, torch::Tensor &gy, torch::Tensor &gr, torch::Tensor &gk, torch::Tensor &gv, torch::Tensor &gw, torch::Tensor &gu) {
-    cuda_backward(B, T, C, H, r.data_ptr<bf16>(), k.data_ptr<bf16>(), v.data_ptr<bf16>(), w.data_ptr<float>(), ww.data_ptr<float>(), u.data_ptr<bf16>(), gy.data_ptr<bf16>(), gr.data_ptr<bf16>(), gk.data_ptr<bf16>(), gv.data_ptr<bf16>(), gw.data_ptr<bf16>(), gu.data_ptr<bf16>());
-}
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &forward, "wkv5 forward");
-  m.def("backward", &backward, "wkv5 backward");
-}
-
-TORCH_LIBRARY(wkv5, m) {
-  m.def("forward", forward);
-  m.def("backward", backward);
 }
