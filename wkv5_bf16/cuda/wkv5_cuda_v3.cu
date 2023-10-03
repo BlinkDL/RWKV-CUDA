2136ee74ca0b59894e0bfb09273e20486c8f213d
BBuf <1182563586@qq.com>
2023-10-03 12:40:23 +0000

    refine

--- wkv5_bf16/cuda/wkv5_cuda_v3.cu
+++ wkv5_bf16/cuda/wkv5_cuda_v3.cu
@@ -3,88 +3,61 @@
 #include "ATen/ATen.h"
 typedef at::BFloat16 bf16;
 
-template<typename T>
-struct alignas(8 * sizeof(T)) Pack8X {
-  T x, y, z, w, a, b, c, d;
-};
-
 template <typename F>
 __global__ void kernel_forward(const int B, const int T, const int C, const int H,
-                               F *__restrict__ _r, F *__restrict__ _k, const F *__restrict__ const _v, const float *__restrict__ _w, F *__restrict__ _u,
+                               const F *__restrict__ const _r, const F *__restrict__ const _k, const F *__restrict__ const _v, const float *__restrict__ _w, const F *__restrict__ _u,
                                F *__restrict__ const _y)
 {
     const int b = blockIdx.x / H;
-    const int h = blockIdx.x - (blockIdx.x / H) * H;
+    const int h = blockIdx.x % H;
     const int i = threadIdx.x;
     _w += h*_N_;
     _u += h*_N_;
 
-    Pack8X<F>* k = reinterpret_cast<Pack8X<F>*>(_k);
-    Pack8X<F>* r = reinterpret_cast<Pack8X<F>*>(_r);
-    const float4 *__restrict__ const w = (float4 *)(_w);
-    Pack8X<F>* u = reinterpret_cast<Pack8X<F>*>(_u);
-    const F *__restrict__ const v = _v;
-    F *__restrict__ const y = _y;
+    __shared__ float r[_N_], k[_N_], u[_N_], w[_N_];
+    float state[_N_] = {0};
 
-    __align__(16) float4 state[_N_ >> 2] = { make_float4(0.0f, 0.0f, 0.0f, 0.0f) };
-    for ( int  i =0 ;i < _N_ >> 2; i++){
-        state[i] = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
-    }
-    __shared__ Pack8X<F> rr[_N_ >> 3], kk[_N_ >> 3];
+    __syncthreads();
+    u[i] = float(_u[i]);
+    w[i] = float(_w[i]);
+    __syncthreads();
 
-    for (int _tt = b*T*C + h*_N_ + i, _tend = (b+1)*T*C + h*_N_ + i; _tt < _tend; _tt += C)
+    for (int t = b*T*C + h*_N_ + i; t < (b+1)*T*C + h*_N_ + i; t += C)
     {
-        const int _t = _tt >> 3;
-        const float vv = float(v[_tt]);
-        float yy = 0.0;
-        
         __syncthreads();
-        rr[i >> 3] = r[_t];
-        kk[i >> 3] = k[_t];
+        r[i] = float(_r[t]);
+        k[i] = float(_k[t]);
         __syncthreads();
+
+        const float v = float(_v[t]);
+        float y = 0;
 
         #pragma unroll
-        for (int j = 0; j < _N_ >> 3; j++)
+        for (int j = 0; j < _N_; j+=4)
         {
-            const Pack8X<F> uu = u[j];
-            const Pack8X<F> rrr = rr[j];
-            const Pack8X<F> kkk = kk[j];
+            const float4& r_ = (float4&)(r[j]);
+            const float4& k_ = (float4&)(k[j]);
+            const float4& w_ = (float4&)(w[j]);
+            const float4& u_ = (float4&)(u[j]);
+            float4& s = (float4&)(state[j]);
+            float4 x;
 
-            float4 x1;
-            x1.x = float(kkk.x) * vv;
-            x1.y = float(kkk.y) * vv;
-            x1.z = float(kkk.z) * vv;
-            x1.w = float(kkk.w) * vv;
-            float4 &s1 = state[j<<1];
-            const float4 ww1 = w[j<<1];
+            x.x = k_.x * v;
+            x.y = k_.y * v;
+            x.z = k_.z * v;
+            x.w = k_.w * v;
 
-            yy += float(rrr.x) * (float(uu.x) * x1.x + s1.x);
-            yy += float(rrr.y) * (float(uu.y) * x1.y + s1.y);
-            yy += float(rrr.z) * (float(uu.z) * x1.z + s1.z);
-            yy += float(rrr.w) * (float(uu.w) * x1.w + s1.w);
-            s1.x = s1.x * ww1.x + x1.x;
-            s1.y = s1.y * ww1.y + x1.y;
-            s1.z = s1.z * ww1.z + x1.z;
-            s1.w = s1.w * ww1.w + x1.w;
+            y += r_.x * (u_.x * x.x + s.x);
+            y += r_.y * (u_.y * x.y + s.y);
+            y += r_.z * (u_.z * x.z + s.z);
+            y += r_.w * (u_.w * x.w + s.w);
 
-            float4 x2;
-            x2.x = float(kkk.a) * vv;
-            x2.y = float(kkk.b) * vv;
-            x2.z = float(kkk.c) * vv;
-            x2.w = float(kkk.d) * vv;
-            float4 &s2 = state[j<<1|1];
-            const float4 ww2 = w[j<<1|1];
-            
-            yy += float(rrr.a) * (float(uu.a) * x2.x + s2.x);
-            yy += float(rrr.b) * (float(uu.b) * x2.y + s2.y);
-            yy += float(rrr.c) * (float(uu.c) * x2.z + s2.z);
-            yy += float(rrr.d) * (float(uu.d) * x2.w + s2.w);
-            s2.x = s2.x * ww2.x + x2.x;
-            s2.y = s2.y * ww2.y + x2.y;
-            s2.z = s2.z * ww2.z + x2.z;
-            s2.w = s2.w * ww2.w + x2.w;
+            s.x = s.x * w_.x + x.x;
+            s.y = s.y * w_.y + x.y;
+            s.z = s.z * w_.z + x.z;
+            s.w = s.w * w_.w + x.w;
         }
-        y[_tt] = F(yy);
+        _y[t] = F(y);
     }
 }
 
